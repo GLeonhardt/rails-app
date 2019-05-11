2f3838cff0f23b3e14e1b9851d26ef362c4b3440
GLeonhardt <guileonhardt2@gmail.com>
2019-05-11 11:32:29 -0300

    Work in progress

---
+++ test/models/contact_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class ContactTest < ActiveSupport::TestCase
+  test "don't create a contact withou name" do
+    assert_difference 'User.all.count', 0 do
+      User.first.contacts.create born_date:'anything'
+    end
+  end
+end
